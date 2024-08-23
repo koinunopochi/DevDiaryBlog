22797f92041622c8109b2fd6739736f67bf6388c
Koinunopochi <koinunopoti@gmail.com>
2024-08-23 14:00:33 +0900

    fix : なぜなら、「emailのフォーマットを検証する」ために、「バリデーションを追加」した

--- src/laravel/app/Domain/ValueObjects/Email.php
+++ src/laravel/app/Domain/ValueObjects/Email.php
@@ -2,13 +2,24 @@
 
 namespace App\Domain\ValueObjects;
 
+use InvalidArgumentException;
+
 class Email
 {
   private string $email;
 
   public function __construct(string $email)
   {
+    $this->validate($email);
     $this->email = $email;
+  }
+
+  private function validate(string $email): void
+  {
+    $canParseEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if (!$canParseEmail) {
+      throw new InvalidArgumentException('emailの形式が不正です');
+    }
   }
 
   public function toString(): string
