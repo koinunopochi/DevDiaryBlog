9093aad1e5ef31d21ac6e677bedbc1369c2cfbc0
Koinunopochi <koinunopoti@gmail.com>
2024-08-23 14:39:27 +0900

    Lint : なぜなら、「Linterを行い忘れた」ために、「Linterを」した

--- src/laravel/tests/Unit/Domain/ValueObjects/UserIdTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/UserIdTest.php
@@ -5,33 +5,34 @@ namespace Tests\Unit\Domain\ValueObjects;
 use Tests\TestCase;
 use App\Domain\ValueObjects\UserId;
 use Ramsey\Uuid\Uuid;
+
 class UserIdTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function testToString(): void
-    {
-        // Given
-        $userId = "user-" . Uuid::uuid4()->toString();
+  /**
+   * @test
+   */
+  public function testToString(): void
+  {
+    // Given
+    $userId = "user-" . Uuid::uuid4()->toString();
 
-        // When
-        $userIdValueObject = new UserId($userId);
+    // When
+    $userIdValueObject = new UserId($userId);
 
-        // Then
-        $this->assertEquals($userId, $userIdValueObject->toString());
-    }
+    // Then
+    $this->assertEquals($userId, $userIdValueObject->toString());
+  }
 
-    /**
-     * @test
-     */
-    public function testInvalidUserIdFormat(): void
-    {
-        // Given
-        $userId = "user-invalid-user-id-format";
+  /**
+   * @test
+   */
+  public function testInvalidUserIdFormat(): void
+  {
+    // Given
+    $userId = "user-invalid-user-id-format";
 
-        // When & Then
-        $this->expectException(\InvalidArgumentException::class);
-        new UserId($userId);
-    }
+    // When & Then
+    $this->expectException(\InvalidArgumentException::class);
+    new UserId($userId);
+  }
 }
