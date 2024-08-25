1499c902718d96a435779289749d8ae1d4fdbff8
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 02:26:20 +0900

    test : なぜなら、「実行されないことを確認する」ために、「最低限のテストを作成」した

---
+++ src/laravel/tests/Unit/Domain/Entities/UserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit\Domain\Entities;
+
+use App\Domain\Entities\User;
+use App\Domain\ValueObjects\Email;
+use App\Domain\ValueObjects\Password;
+use App\Domain\ValueObjects\UserId;
+use App\Domain\ValueObjects\Username;
+use App\Domain\ValueObjects\UserStatus;
+use DateTime;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+  /**
+   * @test
+   */
+  public function testUserCanBeInstantiated(): void
+  {
+    // Given
+    $userId = new UserId();
+    $username = new Username('testuser');
+    $emailAddress = new Email('test@example.com');
+    $hashedPassword = new Password('password*A1aaa');
+    $userStatus = new UserStatus(UserStatus::STATUS_ACTIVE);
+    $createdAt = new DateTime();
+    $updatedAt = new DateTime();
+
+    // When
+    $user = new User(
+      $userId,
+      $username,
+      $emailAddress,
+      $hashedPassword,
+      $userStatus,
+      $createdAt,
+      $updatedAt
+    );
+
+    // Then
+    $this->assertEquals($userId, $user->getUserId());
+    $this->assertEquals($username, $user->getUsername());
+    $this->assertEquals($emailAddress, $user->getEmail());
+    $this->assertEquals($hashedPassword, $user->getPassword());
+    $this->assertEquals($userStatus, $user->getStatus());
+    $this->assertEquals($createdAt, $user->getCreatedAt());
+    $this->assertEquals($updatedAt, $user->getUpdatedAt());
+  }
+}
