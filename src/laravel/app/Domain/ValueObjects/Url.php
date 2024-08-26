b5abaf78e4e261536757a86cf1938bf15e6a9fa6
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 19:53:26 +0900

    fix : なぜなら、「Urlか判断する」ために、「バリデーションを」追加した
    一部バリデーションが甘いが、大きな問題にはならないためざっくりしたバリデーションとする

--- src/laravel/app/Domain/ValueObjects/Url.php
+++ src/laravel/app/Domain/ValueObjects/Url.php
@@ -2,13 +2,24 @@
 
 namespace App\Domain\ValueObjects;
 
+use InvalidArgumentException;
+
 class Url
 {
   private string $url;
 
   public function __construct(string $url)
   {
+    $this->validate($url);
     $this->url = $url;
+  }
+
+  private function validate(string $url): void
+  {
+    // note: htpとhttが通ってしまう問題があるが、大きな問題ではないため放置する
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+      throw new InvalidArgumentException('URLの形式が正しくありません: ' . $url);
+    }
   }
 
   public function toString(): string
