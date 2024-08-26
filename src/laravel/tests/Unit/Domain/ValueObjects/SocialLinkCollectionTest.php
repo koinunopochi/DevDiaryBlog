d81ccde0bd8ef593edffef7a7f13299aaa801f44
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 20:08:28 +0900

    test : なぜなら、「実行されないことを確認する」ために、「最低限のテストを作成」した

---
+++ src/laravel/tests/Unit/Domain/ValueObjects/SocialLinkCollectionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Domain\ValueObjects;
+
+use Tests\TestCase;
+use App\Domain\ValueObjects\SocialLinkCollection;
+
+class SocialLinkCollectionTest extends TestCase
+{
+  /** @test */
+  public function testCreateInstance()
+  {
+    // Given
+    $socialLinks = [
+      'twitter' => 'https://twitter.com/example',
+    ];
+
+    // When
+    $socialLinkCollection = new SocialLinkCollection($socialLinks);
+
+    // Then
+    $this->assertInstanceOf(SocialLinkCollection::class, $socialLinkCollection);
+  }
+}
