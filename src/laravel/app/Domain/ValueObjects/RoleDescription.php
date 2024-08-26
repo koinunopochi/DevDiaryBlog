07bee3a430656a2f2efa7d87d1a3ebaa8c845190
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 23:59:06 +0900

    fix : なぜなら、「正しい文字列長か判断する」ために、「バリデーションを」追加した

--- src/laravel/app/Domain/ValueObjects/RoleDescription.php
+++ src/laravel/app/Domain/ValueObjects/RoleDescription.php
@@ -10,11 +10,19 @@ class RoleDescription
 
   public function __construct(string $roleDescription)
   {
+    $this->validate($roleDescription);
     $this->roleDescription = $roleDescription;
   }
 
   public function toString(): string
   {
     return $this->roleDescription;
+  }
+
+  private function validate(string $roleDescription): void
+  {
+    if (mb_strlen($roleDescription) > 255) {
+      throw new InvalidArgumentException('Roleの説明は255文字以内で入力してください');
+    }
   }
 }
