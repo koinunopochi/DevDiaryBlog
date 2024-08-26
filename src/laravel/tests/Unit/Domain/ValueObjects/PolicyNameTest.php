7db1040fa098856c1c56d37e14c36217fbf637df
Koinunopochi <koinunopoti@gmail.com>
2024-08-27 01:33:17 +0900

    test : なぜなら、「バリデーションが行われているか確認する」ために、「テストを作成」した

--- src/laravel/tests/Unit/Domain/ValueObjects/PolicyNameTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/PolicyNameTest.php
@@ -33,4 +33,25 @@ class PolicyNameTest extends TestCase
     // Then
     $this->assertEquals($policyNameString, $result);
   }
+
+
+  /** @test */
+  public function testValidate()
+  {
+    // 0文字
+    $this->expectException(\InvalidArgumentException::class);
+    new PolicyName('');
+
+    // 1文字
+    $policyName = new PolicyName('a');
+    $this->assertInstanceOf(PolicyName::class, $policyName);
+
+    // 50文字
+    $policyName = new PolicyName(str_repeat('a', 50));
+    $this->assertInstanceOf(PolicyName::class, $policyName);
+
+    // 51文字
+    $this->expectException(\InvalidArgumentException::class);
+    new PolicyName(str_repeat('a', 51));
+  }
 }
