b99394443b84844639fb4b499ccd93a832d08127
Koinunopochi <koinunopoti@gmail.com>
2024-08-27 01:09:01 +0900

    fix : なぜなら、「正しい文字列長か判断する」ために、「バリデーションを」追加した

--- src/laravel/app/Domain/ValueObjects/PolicyDescription.php
+++ src/laravel/app/Domain/ValueObjects/PolicyDescription.php
@@ -8,7 +8,15 @@ class PolicyDescription
 
   public function __construct(string $description)
   {
+    $this->validate($description);
     $this->description = $description;
+  }
+
+  private function validate(string $description): void
+  {
+    if (mb_strlen($description) > 255) {
+      throw new \InvalidArgumentException('Policy description は255文字以下である必要があります。');
+    }
   }
 
   public function toString(): string
