98a987005b4c3aa0d5321ca3b0a06076c07ad98b
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 23:49:00 +0900

    fix : なぜなら、「正しい文字列長か判断する」ために、「バリデーションを」追加した

--- src/laravel/app/Domain/ValueObjects/RoleName.php
+++ src/laravel/app/Domain/ValueObjects/RoleName.php
@@ -8,7 +8,15 @@ class RoleName
 
   public function __construct(string $roleName)
   {
+    $this->validate($roleName);
     $this->roleName = $roleName;
+  }
+
+  private function validate(string $roleName): void
+  {
+    if (mb_strlen($roleName) < 1 || mb_strlen($roleName) > 50) {
+      throw new \InvalidArgumentException('RoleNameは1文字以上50文字以下である必要があります。');
+    }
   }
 
   public function toString(): string
