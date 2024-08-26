4f76461690651d491e896ef0b1b37f53d72dd54a
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 20:21:01 +0900

    test : なぜなら、「正しくバリデーションができるか確認する」ために、「テストを作成」した

--- src/laravel/tests/Unit/Domain/ValueObjects/SocialLinkCollectionTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/SocialLinkCollectionTest.php
@@ -36,4 +36,58 @@ class SocialLinkCollectionTest extends TestCase
     // Then
     $this->assertEquals($socialLinks, $socialLinkCollection->toArray());
   }
+
+  /** @test */
+  public function testValidate_KeyLengthTooLong()
+  {
+    // Given
+    $socialLinks = [
+      str_repeat('a', 51) => 'https://example.com',
+    ];
+
+    // When & Then
+    $this->expectException(\InvalidArgumentException::class);
+    new SocialLinkCollection($socialLinks);
+  }
+
+  /** @test */
+  public function testValidate_KeyIsEmpty()
+  {
+    // Given
+    $socialLinks = [
+      '' => 'https://example.com',
+    ];
+
+    // When & Then
+    $this->expectException(\InvalidArgumentException::class);
+    new SocialLinkCollection($socialLinks);
+  }
+
+  /** @test */
+  public function testValidate_KeyLengthOk()
+  {
+    // Given
+    $socialLinks = [
+      str_repeat('a', 50) => 'https://example.com',
+    ];
+
+    // When
+    $socialLinkCollection = new SocialLinkCollection($socialLinks);
+
+    // Then
+    $this->assertInstanceOf(SocialLinkCollection::class, $socialLinkCollection);
+  }
+
+  /** @test */
+  public function testValidate_InvalidUrl()
+  {
+    // Given
+    $socialLinks = [
+      'twitter' => 'invalid-url',
+    ];
+
+    // When & Then
+    $this->expectException(\InvalidArgumentException::class);
+    new SocialLinkCollection($socialLinks);
+  }
 }
