840f0481e7a2fd66f5d305b524403cc72840a795
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 23:48:24 +0900

    test : なぜなら、「バリデーションが行われているか確認する」ために、「テストを作成」した

--- src/laravel/tests/Unit/Domain/ValueObjects/RoleNameTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/RoleNameTest.php
@@ -37,4 +37,36 @@ class RoleNameTest extends TestCase
     // Then
     $this->assertSame($roleNameString, $result);
   }
+
+  /**
+   * @test
+   * @dataProvider roleNameProvider
+   */
+  public function testValidate(int $length, bool $isValid)
+  {
+    // Given
+    $roleNameString = str_repeat('a', $length);
+
+    // When
+    if (!$isValid) {
+      $this->expectException(\InvalidArgumentException::class);
+    }
+
+    $roleName = new RoleName($roleNameString);
+
+    // Then
+    if ($isValid) {
+      $this->assertInstanceOf(RoleName::class, $roleName);
+    }
+  }
+
+  public function roleNameProvider(): array
+  {
+    return [
+      '0文字はエラー' => [0, false],
+      '1文字は正常' => [1, true],
+      '50文字は正常' => [50, true],
+      '51文字はエラー' => [51, false],
+    ];
+  }
 }
