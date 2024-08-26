bbda1f63c061adf2bec67307f8dc83b1b0b26e77
Koinunopochi <koinunopoti@gmail.com>
2024-08-27 01:08:50 +0900

    test : なぜなら、「バリデーションが行われているか確認する」ために、「テストを作成」した

--- src/laravel/tests/Unit/Domain/ValueObjects/PolicyDescriptionTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/PolicyDescriptionTest.php
@@ -36,4 +36,32 @@ class PolicyDescriptionTest extends TestCase
     // Then
     $this->assertEquals($policyDescription, $policyDescriptionValueObject->toString());
   }
+
+  /**
+   * @test
+   */
+  public function testValidLength(): void
+  {
+    // Given
+    $policyDescription = str_repeat('a', 255);
+
+    // When
+    $policyDescriptionValueObject = new PolicyDescription($policyDescription);
+
+    // Then
+    $this->assertInstanceOf(PolicyDescription::class, $policyDescriptionValueObject);
+  }
+
+  /**
+   * @test
+   */
+  public function testInvalidLength(): void
+  {
+    // Given
+    $policyDescription = str_repeat('a', 256);
+
+    // When & Then
+    $this->expectException(\InvalidArgumentException::class);
+    new PolicyDescription($policyDescription);
+  }
 }
