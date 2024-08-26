0e2e800117f6492963b02f9d1b8cd21b38b16166
Koinunopochi <koinunopoti@gmail.com>
2024-08-26 19:18:37 +0900

    fix : なぜなら、「指定の文字列長で許可する」ために、「バリデーション」を追加した

--- src/laravel/app/Domain/ValueObjects/UserBio.php
+++ src/laravel/app/Domain/ValueObjects/UserBio.php
@@ -8,11 +8,19 @@ class UserBio
 
   public function __construct(string $userBio)
   {
+    $this->validate($userBio);
     $this->userBio = $userBio;
   }
 
   public function toString(): string
   {
     return $this->userBio;
+  }
+
+  private function validate(string $userBio): void
+  {
+    if (mb_strlen($userBio) > 500) {
+      throw new \InvalidArgumentException('自己紹介は500文字以内で入力してください。');
+    }
   }
 }
