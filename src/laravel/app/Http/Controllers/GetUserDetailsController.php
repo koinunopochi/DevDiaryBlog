4500bb32b8b6d046f064f3048cc29816fdfdb2b5
Koinunopochi <koinunopoti@gmail.com>
2024-09-03 22:09:19 +0900

    feature : なぜなら、「値を取得する」ため、「コントローラー」を作成した

---
+++ src/laravel/app/Http/Controllers/GetUserDetailsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Application\Services\GetUserDetailsService;
+use App\Domain\ValueObjects\UserId;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class GetUserDetailsController extends Controller
+{
+  private GetUserDetailsService $getUserDetailsService;
+
+  public function __construct(GetUserDetailsService $getUserDetailsService)
+  {
+    $this->getUserDetailsService = $getUserDetailsService;
+  }
+
+  public function execute(Request $request, string $userId): JsonResponse
+  {
+    try {
+      $userDetails = $this->getUserDetailsService->execute(new UserId($userId));
+
+      if (!$userDetails) {
+        return new JsonResponse(['error' => 'User not found'], 404);
+      }
+
+      return new JsonResponse($userDetails->toArray());
+    } catch (Exception $e) {
+
+      return new JsonResponse(['error' => $e->getMessage()], 500);
+    }
+  }
+}
