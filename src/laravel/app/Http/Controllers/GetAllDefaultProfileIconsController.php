ef3b873576381a08a333eb10a0aef503a6b5fdb7
Koinunopochi <koinunopoti@gmail.com>
2024-09-05 23:09:06 +0900

    feature : なぜなら、「apiを利用してアクセスできるようにする」ために、コントローラーの作成とルートの開放を行った

---
+++ src/laravel/app/Http/Controllers/GetAllDefaultProfileIconsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Application\UseCases\GetAllDefaultProfileIconsUseCase;
+
+class GetAllDefaultProfileIconsController extends Controller
+{
+
+  private GetAllDefaultProfileIconsUseCase $allDefaultProfileIconsUseCase;
+  public function __construct(GetAllDefaultProfileIconsUseCase $getAllDefaultProfileIconsUseCase)
+  {
+    $this->allDefaultProfileIconsUseCase = $getAllDefaultProfileIconsUseCase;
+  }
+
+  public function execute()
+  {
+    try {
+      $result = $this->allDefaultProfileIconsUseCase->execute();
+      return response()->json(['icons'=>$result]);
+    } catch (\Exception $e) {
+      return response()->json(['error' =>  $e->getMessage()], 500);
+    }
+  }
+}
