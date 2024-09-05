8666cbfe4f1bb5038ee51c7053ca18a748bb1f17
Koinunopochi <koinunopoti@gmail.com>
2024-09-05 18:33:04 +0900

    test : なぜなら、「最低限指定したバケットのディレクトリから値を取得できるか確かめる」ために、「テスト」を作成した

---
+++ src/laravel/tests/Feature/Infrastructure/Persistence/MinioProfileIconRepositoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Infrastructure\Persistence;
+
+use App\Infrastructure\Persistence\MinioProfileIconRepository;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class MinioProfileIconRepositoryTest extends TestCase
+{
+  protected function setUp(): void
+  {
+    parent::setUp();
+    Storage::fake('s3');
+    config()->set('logging.default', 'stderr');
+  }
+
+  public function testGetDefaultAll()
+  {
+    // Given
+    Storage::disk('s3')->put('/profile-icons/defaults/icon1.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon2.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon3.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon4.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon5.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon6.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon7.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon8.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon9.png', 'test');
+    Storage::disk('s3')->put('/profile-icons/defaults/icon10.png', 'test');
+    
+    $repository = new MinioProfileIconRepository();
+
+    // When
+    $icons = $repository->getDefaultAll();
+
+    // Then
+    $this->assertCount(10, $icons);
+  }
+}
