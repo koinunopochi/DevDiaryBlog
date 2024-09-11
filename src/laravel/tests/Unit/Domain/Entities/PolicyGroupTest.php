62d0dde8d6534eaa17127fd57bbba0dfd0a0aae7
Koinunopochi <koinunopoti@gmail.com>
2024-09-12 04:23:37 +0900

    test : なぜなら、「 差し込み：PolicyGroupを作成することで、Policyの管理を容易にする #272 」ため、「 PolicyGroupの最低限のテストを 」 作成 した

---
+++ src/laravel/tests/Unit/Domain/Entities/PolicyGroupTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Domain\Entities;
+
+use App\Domain\Entities\PolicyGroup;
+use App\Domain\ValueObjects\PolicyGroupDescription;
+use App\Domain\ValueObjects\PolicyGroupId;
+use App\Domain\ValueObjects\PolicyGroupName;
+use App\Domain\ValueObjects\PolicyId;
+use App\Domain\ValueObjects\PolicyIdCollection;
+use Tests\TestCase;
+
+class PolicyGroupTest extends TestCase
+{
+  /** @test */
+  public function testCreateInstance()
+  {
+    // Given
+    $policyGroupId = new PolicyGroupId();
+    $policyGroupName = new PolicyGroupName('テストポリシーグループ');
+    $policyGroupDescription = new PolicyGroupDescription('簡単な説明');
+    $policyId_1 = new PolicyId();
+    $policyId_2 = new PolicyId();
+    $policies = new PolicyIdCollection([$policyId_1, $policyId_2]);
+
+    // When
+    $policyGroup = new PolicyGroup($policyGroupId, $policyGroupName, $policyGroupDescription, $policies);
+
+    // Then
+    $this->assertInstanceOf(PolicyGroup::class, $policyGroup);
+    $this->assertEquals($policyGroupId, $policyGroup->getId());
+    $this->assertEquals($policyGroupName, $policyGroup->getName());
+    $this->assertEquals($policies, $policyGroup->getPolicies());
+  }
+}
+
