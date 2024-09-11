c5e864a9df0b7507780a4b6bd0382c744f7c6f28
Koinunopochi <koinunopoti@gmail.com>
2024-09-12 04:26:14 +0900

    test : なぜなら、「 差し込み：PolicyGroupを作成することで、Policyの管理を容易にする #272 」ため、「 PolicyGroupのテストにポリシーの追加と削除を 」 追加 した

--- src/laravel/tests/Unit/Domain/Entities/PolicyGroupTest.php
+++ src/laravel/tests/Unit/Domain/Entities/PolicyGroupTest.php
@@ -32,5 +32,48 @@ class PolicyGroupTest extends TestCase
     $this->assertEquals($policyGroupName, $policyGroup->getName());
     $this->assertEquals($policies, $policyGroup->getPolicies());
   }
-}
 
+  /** @test */
+  public function testAddPolicy()
+  {
+    // Given
+    $policyGroupId = new PolicyGroupId();
+    $policyGroupName = new PolicyGroupName('テストポリシーグループ');
+    $policyGroupDescription = new PolicyGroupDescription('簡単な説明');
+    $policyId_1 = new PolicyId();
+    $policyId_2 = new PolicyId();
+    $policies = new PolicyIdCollection([$policyId_1]);
+
+    // When
+    $policyGroup = new PolicyGroup($policyGroupId, $policyGroupName, $policyGroupDescription, $policies);
+    $policyGroup->addPolicy($policyId_2);
+
+    // Then
+    $this->assertInstanceOf(PolicyGroup::class, $policyGroup);
+    $this->assertEquals($policyGroupId, $policyGroup->getId());
+    $this->assertEquals($policyGroupName, $policyGroup->getName());
+    $this->assertCount(2, $policyGroup->getPolicies()->toArray());
+  }
+
+  /** @test */
+  public function testRemovePolicy()
+  {
+    // Given
+    $policyGroupId = new PolicyGroupId();
+    $policyGroupName = new PolicyGroupName('テストポリシーグループ');
+    $policyGroupDescription = new PolicyGroupDescription('簡単な説明');
+    $policyId_1 = new PolicyId();
+    $policyId_2 = new PolicyId();
+    $policies = new PolicyIdCollection([$policyId_1, $policyId_2]);
+
+    // When
+    $policyGroup = new PolicyGroup($policyGroupId, $policyGroupName, $policyGroupDescription, $policies);
+    $policyGroup->removePolicy($policyId_2);
+
+    // Then
+    $this->assertInstanceOf(PolicyGroup::class, $policyGroup);
+    $this->assertEquals($policyGroupId, $policyGroup->getId());
+    $this->assertEquals($policyGroupName, $policyGroup->getName());
+    $this->assertCount(1, $policyGroup->getPolicies()->toArray());
+  }
+}
