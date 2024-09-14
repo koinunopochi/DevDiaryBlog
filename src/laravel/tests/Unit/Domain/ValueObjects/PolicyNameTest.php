80791721c5993c4ac4bd7e907c7142a30e6e1503
Koinunopochi <koinunopoti@gmail.com>
2024-09-15 00:26:32 +0900

    test : なぜなら、「 fix : Policyの文字数制限が日本語の場合制限が2倍厳しくなっている点を修正する #279 」ため、「 エラーが正しく表示されなかったため、正しくテストが行われるように 」 テスト を修正した

--- src/laravel/tests/Unit/Domain/ValueObjects/PolicyNameTest.php
+++ src/laravel/tests/Unit/Domain/ValueObjects/PolicyNameTest.php
@@ -10,76 +10,61 @@ class PolicyNameTest extends TestCase
   /** @test */
   public function testCreateInstance()
   {
-    // Given
     $policyNameString = 'テストポリシー名';
-
-    // When
     $policyName = new PolicyName($policyNameString);
 
-    // Then
     $this->assertInstanceOf(PolicyName::class, $policyName);
   }
 
   /** @test */
   public function testToString()
   {
-    // Given
     $policyNameString = 'テストポリシー名';
     $policyName = new PolicyName($policyNameString);
 
-    // When
     $result = $policyName->toString();
 
-    // Then
     $this->assertEquals($policyNameString, $result);
   }
 
-
-  /** @test */
-  public function testValidate()
+  /**
+   * @test
+   * @dataProvider provideValidPolicyNames
+   */
+  public function testValidPolicyNames($policyNameString)
   {
-    // 0文字
-    $this->expectException(\InvalidArgumentException::class);
-    new PolicyName('');
-
-    // 1文字
-    $policyName = new PolicyName('a');
+    $policyName = new PolicyName($policyNameString);
     $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 50文字
-    $policyName = new PolicyName(str_repeat('a', 50));
-    $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 51文字
-    $this->expectException(\InvalidArgumentException::class);
-    new PolicyName(str_repeat('a', 51));
   }
 
-  /** @test */
-  public function test2byteValidate()
+  public static function provideValidPolicyNames()
   {
-    // 0文字
+    return [
+      ['a'],                                                   // 1文字
+      [str_repeat('a', 50)],                    // 50文字 (半角)
+      ['あ'],                                                  // 1文字 (全角)
+      ['ユーザー自己情報読み取り（パターン一致）'],              // 本番のような文字
+      [str_repeat('あ', 50)],                   // 100バイト (全角)   50文字となる
+      [str_repeat('（', 50)],                   // 50文字 (記号)
+    ];
+  }
+
+  /**
+   * @test
+   * @dataProvider provideInvalidPolicyNames
+   */
+  public function testInvalidPolicyNames($policyNameString)
+  {
     $this->expectException(\InvalidArgumentException::class);
-    new PolicyName('');
+    new PolicyName($policyNameString);
+  }
 
-    // 1文字
-    $policyName = new PolicyName('あ');
-    $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 本番のような文字
-    $policyName = new PolicyName('ユーザー自己情報読み取り（パターン一致）');
-    $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 50文字
-    $policyName = new PolicyName(str_repeat('あ', 50));
-    $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 50文字（記号）
-    $policyName = new PolicyName(str_repeat('（', 50));
-    $this->assertInstanceOf(PolicyName::class, $policyName);
-
-    // 51文字
-    $this->expectException(\InvalidArgumentException::class);
-    new PolicyName(str_repeat('あ', 51));
+  public static function provideInvalidPolicyNames()
+  {
+    return [
+      [''],                                     // 0文字
+      [str_repeat('a', 51)],                    // 51文字 (半角)
+      [str_repeat('あ', 51)],                    // 102バイト (全角)
+    ];
   }
 }
