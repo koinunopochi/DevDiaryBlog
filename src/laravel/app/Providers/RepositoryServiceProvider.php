65e71e7ff8cf68a27a2393c49f2078f8f74887c6
Koinunopochi <koinunopoti@gmail.com>
2024-09-29 06:38:30 +0900

    feature : なぜなら、「 記事の作成、編集、削除、公開 #352 」ため、「 リポジトリと実態を結びつけるようにプロバイダーに 」 追加 した

--- src/laravel/app/Providers/RepositoryServiceProvider.php
+++ src/laravel/app/Providers/RepositoryServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
+use App\Domain\Repositories\ArticleRepositoryInterface;
 use App\Domain\Repositories\OgpRepositoryInterface;
 use App\Domain\Repositories\ProfileIconRepositoryInterface;
 use App\Domain\Repositories\UserProfileRepositoryInterface;
 use App\Domain\Repositories\UserRepositoryInterface;
 use App\Infrastructure\Ogp\HttpOgpRepository;
+use App\Infrastructure\Persistence\EloquentArticleRepository;
 use App\Infrastructure\Persistence\EloquentUserProfileRepository;
 use App\Infrastructure\Persistence\EloquentUserRepository;
 use App\Infrastructure\Persistence\MinioProfileIconRepository;
@@ -23,6 +25,7 @@ class RepositoryServiceProvider extends ServiceProvider
     $this->app->bind(UserProfileRepositoryInterface::class, EloquentUserProfileRepository::class);
     $this->app->bind(ProfileIconRepositoryInterface::class, MinioProfileIconRepository::class);
     $this->app->bind(OgpRepositoryInterface::class, HttpOgpRepository::class);
+    $this->app->bind(ArticleRepositoryInterface::class, EloquentArticleRepository::class);
   }
 
   /**
